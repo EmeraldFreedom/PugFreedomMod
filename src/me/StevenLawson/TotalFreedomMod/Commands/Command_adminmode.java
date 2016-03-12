32b1d22db465d92ca8087f4b3590efd4e6dc2f7c
rovertdude <tsannoyingorange@gmail.com>
2016-03-12 08:24:21 -0600

    Update Command_adminmode.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_adminmode.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_adminmode.java
@@ -7,7 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.ONLY_CONSOLE, blockHostConsole = true)
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH, blockHostConsole = false)
 @CommandParameters(description = "Close server to non-superadmins.", usage = "/<command> [on | off]")
 public class Command_adminmode extends TFM_Command
 {
@@ -33,7 +33,7 @@ public class Command_adminmode extends TFM_Command
             {
                 if (!TFM_AdminList.isSuperAdmin(player))
                 {
-                    player.kickPlayer("Server is now closed to non-superadmins.");
+                    player.kickPlayer("Admin Mode has been enabled. Please relog in a few minutes or so or check the forums.");
                 }
             }
             return true;
